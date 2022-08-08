56b83e5721a4209ff260b551e1c17a085b5cd7f0
unknown <shashank080311@gmail.com>
2022-08-08 08:57:25 +0530

    enabling

--- app.js
+++ app.js
@@ -1,52 +1,54 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import express from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
-import mongoose from 'mongoose';
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import mongoose from "mongoose";
 
-import getAllUserActionLinks from "./server/Routes/getLinks.js"
+import getAllUserActionLinks from "./server/Routes/getLinks.js";
 const app = express();
 const PORT = process.env.PORT || 3001;
 const URI = `mongodb+srv://virtualLab:${process.env.DB_PASSWORD}@cluster0.e6bxe.mongodb.net/?retryWrites=true&w=majority`;
 app.use(helmet());
-app.use(cors());
-app.use(express.json({ limit: '30mb', extended: true }));
-app.use(express.urlencoded({ limit: '30mb', extended: true }));
+var corsOptions = {
+  origin: "http://localhost:3000/",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.options("*", cors());
+app.use(cors(corsOptions));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
-app.use("/links",getAllUserActionLinks);
-
+app.use("/links", getAllUserActionLinks);
 
 mongoose
-    .connect(URI, { useNewUrlParser: true })
-    .then(()=>{
-        console.log("database connected");
-    })
-    .catch(err =>{
-        console.log(err);
+  .connect(URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log("database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/", async (req, res) => {
+  try {
+    res.status(200).json({
+      method: "SERVER",
+      status: res.statusCode,
+      message: "Server Active",
     });
-
-app.get('/', async (req, res) => {
-    try {
-        res.status(200).json({
-            method: 'SERVER',
-            status: res.statusCode,
-            message: 'Server Active',
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            method: 'SERVER',
-            status: res.statusCode,
-            message: 'Server Inactive',
-        });
-    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      method: "SERVER",
+      status: res.statusCode,
+      message: "Server Inactive",
+    });
+  }
 });
-app.listen(PORT , ()=>{
-    if(process.env.NODE_ENV == 'production'){
-
-    }else{
-        console.log(`server running at: ${PORT}`);
-    }
-})
-
+app.listen(PORT, () => {
+  if (process.env.NODE_ENV == "production") {
+  } else {
+    console.log(`server running at: ${PORT}`);
+  }
+});
